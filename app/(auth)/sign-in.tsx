7427be037b773cb2e23f80f1aef2debb9616bbd6
Yuvi dew <yuvraj@truelink.ai>
2025-06-22 11:38:14 +0530

    refactor: improve code formatting and structure in SignIn component

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,24 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { colors } from '../../assets/colors'
-import InputField from './_components/InputField'
-import CustomButton from '../../components/CustomButton'
-import Oauth from './_components/Oauth'
-import { Link } from 'expo-router'
+import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { colors } from "../../assets/colors";
+import InputField from "./_components/InputField";
+import CustomButton from "../../components/CustomButton";
+import Oauth from "./_components/Oauth";
+import { Link } from "expo-router";
+import { AuthFormType } from "../../types/type";
 
 const SignIn = () => {
-  const [form, setForm] = useState({
+  const [form, setForm] = useState<AuthFormType>({
     email: "",
-    password: ""
-  })
+    password: "",
+    role: "user",
+  });
   return (
     <SafeAreaView style={styles.container}>
       {/* start to top heading */}
       <View style={styles.topBox}>
-        <Text style={styles.headline}>
-          Hello there 👋
-        </Text>
+        <Text style={styles.headline}>Hello there 👋</Text>
         <Text style={styles.text}>
           Please enter your email and password to sign in.
         </Text>
@@ -28,37 +28,39 @@ const SignIn = () => {
       {/* start to sign in form  */}
       <View style={styles.formBox}>
         <InputField
-          label='Email id'
-          placeholder='Enter your email..'
+          label="Email id"
+          placeholder="Enter your email.."
           value={form.email}
           textContentType="emailAddress"
           onChangeText={(value) => setForm({ ...form, email: value })}
         />
 
         <InputField
-          label='Password'
-          placeholder='Enter your password..'
+          label="Password"
+          placeholder="Enter your password.."
           value={form.password}
           textContentType="password"
           onChangeText={(value) => setForm({ ...form, password: value })}
           isPassword
         />
 
-      </View>
-      <CustomButton title='Sign In' />
-      {/* end to sign in form  */}
+        <CustomButton title="Sign in" />
+        {/* end to sign in form  */}
 
-      {/* start to Google Oauth */}
-      <Oauth/>
-      {/* end to Google Oauth */}
+        {/* start to Google Oauth */}
+        <Oauth />
+        {/* end to Google Oauth */}
 
-      <View style = {styles.redirectBox}>
-        <Text style = {styles.redirectBoxText} >Don&apos;t have a account?</Text>
-        <Link href={"/(auth)/sign-up"} style = {styles.redirectBoxLink} >Sign up</Link>
+        <View style={styles.redirectBox}>
+          <Text style={styles.redirectBoxText}>Don&apos;t have a account?</Text>
+          <Link href={"/(auth)/sign-up"} style={styles.redirectBoxLink}>
+            Sign up
+          </Link>
+        </View>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -68,15 +70,13 @@ const styles = StyleSheet.create({
     paddingVertical: 32,
     height: "100%",
     flexDirection: "column",
-    gap: 45,
-  
+    gap: 85,
   },
 
   topBox: {
     flexDirection: "column",
-    gap: 6
+    gap: 6,
   },
-
 
   headline: {
     fontFamily: "Jakarta-Bold",
@@ -88,28 +88,28 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: "Jakarta-Medium",
     color: colors.secondary[800],
-    fontSize: 18
+    fontSize: 18,
   },
 
   formBox: {
     flexDirection: "column",
-    gap: 25
+    gap: 25,
   },
 
-  redirectBox : {
-    flexDirection : "row",
-    alignItems : "center",
-    justifyContent : "center",
-    gap : 3
+  redirectBox: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 3,
   },
 
-  redirectBoxText : {
-    fontFamily : "Jakarta-Medium",
+  redirectBoxText: {
+    fontFamily: "Jakarta-Medium",
   },
-  redirectBoxLink : {
-    fontFamily : "Jakarta-Medium",
-    color : colors.primary[700]
-  }
-})
+  redirectBoxLink: {
+    fontFamily: "Jakarta-Medium",
+    color: colors.primary[700],
+  },
+});
 
-export default SignIn
+export default SignIn;
