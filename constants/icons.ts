42539eec6494362d61a1569be3d184202ac923cf
Yuvi dew <yuvraj@truelink.ai>
2025-06-22 11:39:00 +0530

    feat: add loader icon to icons export for enhanced loading state handling

--- constants/icons.ts
+++ constants/icons.ts
@@ -3,11 +3,13 @@ const leftArrow = require("@/assets/icons/left-arrow.png");
 const closeEye = require("@/assets/icons/hidden.png");
 const openEye = require("@/assets/icons/view.png");
 const google = require("@/assets/icons/google.png");
+const loader = require("@/assets/icons/loader.png")
 
 export const icons = {
     rightArrow,
     leftArrow,
     closeEye,
     openEye,
-    google
+    google,
+    loader
 }
