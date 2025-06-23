f859ecc05161eede7a1160416e5c5d3045d04bfa
Yuvi dew <yuvraj@truelink.ai>
2025-06-24 04:34:40 +0530

    feat: add additional icons for home, explore, booking, and profile components

--- constants/icons.ts
+++ constants/icons.ts
@@ -3,7 +3,15 @@ const leftArrow = require("@/assets/icons/left-arrow.png");
 const closeEye = require("@/assets/icons/hidden.png");
 const openEye = require("@/assets/icons/view.png");
 const google = require("@/assets/icons/google.png");
-const loader = require("@/assets/icons/loader.png")
+const loader = require("@/assets/icons/loader.png");
+const home = require("@/assets/icons/home.png")
+const homeFocus = require("@/assets/icons/home-focus.png")
+const explore = require("@/assets/icons/explore.png")
+const exploreFocus = require("@/assets/icons/explore-focus.png")
+const booking = require("@/assets/icons/booking.png")
+const bookingFocus = require("@/assets/icons/booking-focus.png")
+const profile = require("@/assets/icons/profile.png")
+const profileFocus = require("@/assets/icons/profile-focus.png")
 
 export const icons = {
     rightArrow,
@@ -11,5 +19,13 @@ export const icons = {
     closeEye,
     openEye,
     google,
-    loader
+    loader,
+    home,
+    homeFocus,
+    explore,
+    exploreFocus,
+    booking,
+    bookingFocus,
+    profile,
+    profileFocus
 }
