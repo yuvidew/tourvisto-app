87c22372722dfcc242dbda146b87e3fde7c3c6a5
Yuvi dew <yuvraj@truelink.ai>
2025-06-29 18:54:04 +0530

    feat : creating a filter component

---
+++ app/(root)/_components/Filter.tsx
@@ -0,0 +1,82 @@
+import { View, Text, ScrollView, TouchableOpacity, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { interests } from '../../../constants/interests';
+import { colors } from '../../../assets/colors'
+import { router, useLocalSearchParams } from 'expo-router'
+
+const Filter = () => {
+    const params = useLocalSearchParams<{interests? : string}>()
+    const [selectInterest, setSelectInterest] = useState(params.interests || "Food & Culinary")
+
+    const onInterest = ( value : string) => {
+        setSelectInterest(value)
+        router.setParams({
+            interests : value
+        })
+    }
+
+    return (
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={{ marginTop: 12, marginBottom: 8 }}
+        >
+            {interests.map((item, index) => (
+                <TouchableOpacity
+                    key={index}
+                    style={styles.button}
+                    onPress={() => onInterest(item)}
+                >
+                    <View style={[styles.topline, selectInterest !== item ? styles.topLineBg : styles.topLineBgSelected]} />
+                    <Text style={[styles.text, selectInterest !== item ? styles.textFamily : styles.textFamilySelected]}>
+                        {item}
+                    </Text>
+                </TouchableOpacity>
+            ))}
+        </ScrollView>
+    )
+}
+
+export default Filter
+
+const styles = StyleSheet.create({
+    button: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        marginRight: 16,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        gap: 2
+
+    },
+
+    topline: {
+        width: 30,
+        height: 2,
+    },
+
+    topLineBg: {
+        backgroundColor: colors.secondary[600],
+    },
+
+    topLineBgSelected: {
+        backgroundColor: colors.primary[600],
+    },
+
+    text: {
+        fontSize: 16,
+        color: "#555",
+    },
+
+    textFamily: {
+        fontFamily: "Jakarta-Medium",
+        color: colors.secondary[600]
+    },
+
+    textFamilySelected: {
+        fontFamily: "Jakarta-Bold",
+        color: colors.primary[600]
+
+    }
+})
