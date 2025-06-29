7203bd46359c09bf9a1abf238cc1872a3411bc73
Yuvi dew <yuvraj@truelink.ai>
2025-06-29 18:57:19 +0530

    feat : creating interests file

---
+++ constants/interests.ts
@@ -0,0 +1,13 @@
+export const interests = [
+    "Food & Culinary",
+    "Historical Sites",
+    "Hiking & Nature Walks",
+    "Beaches & Water Activities",
+    "Museums & Art",
+    "Nightlife & Bars",
+    "Photography Spots",
+    "Shopping",
+    "Local Experiences",
+];
+
+export const img = "https://images.unsplash.com/photo-1751104486292-e35ffb54b336?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
