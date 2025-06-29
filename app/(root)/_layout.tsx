6cdfb893ef8e38517771776641c2f0c72231bb8e
Yuvi dew <yuvraj@truelink.ai>
2025-06-29 05:57:00 +0530

    feat: implement authentication check and loading state in AppLayout component

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,6 +1,45 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { Redirect, Slot } from 'expo-router'
+import { useEffect, useState } from 'react'
+import { ActivityIndicator, View } from 'react-native';
 
 export default function AppLayout() {
-    
-    return <Slot/>
+    const [checkingAuth, setCheckingAuth] = useState(true);
+    const [hasToken, setHasToken] = useState<boolean | null>(null);
+
+
+    useEffect(() => {
+        const checkIsToken = async () => {
+            try {
+
+                const isToken = await AsyncStorage.getItem("tourvisto-token")
+
+                setHasToken(!!isToken)
+
+            } catch (error) {
+                setHasToken(false)
+            } finally {
+                setCheckingAuth(false)
+            }
+
+            // if (!isToken) return <Redirect href="/(auth)/welcome" />
+        }
+
+        checkIsToken()
+    }, [])
+
+    if (checkingAuth) {
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <ActivityIndicator size="large" />
+            </View>
+        )
+    }
+
+    if (!hasToken) {
+        // no token → redirect into your auth flow
+        return <Redirect href="/(auth)/welcome" />
+    }
+
+    return <Slot />
 }
