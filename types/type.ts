271cf9581beb633059f9082a136dbbddb3165462
Yuvi dew <yuvraj@truelink.ai>
2025-06-29 05:57:46 +0530

    refactor: remove unused AuthFormType and UseAuthReturnType types

--- types/type.ts
+++ types/type.ts
@@ -25,20 +25,7 @@ export type InputFieldProps = {
     onChangeText: (value: string) => void
 };
 
-export type AuthFormType = {
-    email : string,
-    password : string,
-    username? : string,
-    role : string
-}
 
-export type UseAuthReturnType = {
-    onSignUp: (baseurl: string, form: AuthFormType) => Promise<void>;
-    onSignIn: (baseurl: string, form: AuthFormType) => Promise<void>;
-    loading: boolean;
-    showModal: boolean;
-    responseMsg: { success: boolean; message: string };
-};
 
 export type TabIconProps = {
     focused: boolean;
