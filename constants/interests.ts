4197d79b228c7602e537d632ed247d88b645f9f8
Yuvi dew <yuvraj@truelink.ai>
2025-07-06 18:14:19 +0530

    fix : adding a emogis

--- constants/interests.ts
+++ constants/interests.ts
@@ -1,13 +1,14 @@
 export const interests = [
-    "Food & Culinary",
-    "Historical Sites",
-    "Hiking & Nature Walks",
-    "Beaches & Water Activities",
-    "Museums & Art",
-    "Nightlife & Bars",
-    "Photography Spots",
-    "Shopping",
-    "Local Experiences",
+    "🍲 Food & Culinary",
+    "🏰 Historical Sites",
+    "🥾 Hiking & Nature Walks",
+    "🏖️ Beaches & Water Activities",
+    "🖼️ Museums & Art",
+    "🌃 Nightlife & Bars",
+    "📸 Photography Spots",
+    "🛍️ Shopping",
+    "🌏 Local Experiences",
 ];
+
 
 export const img = "https://images.unsplash.com/photo-1751104486292-e35ffb54b336?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
