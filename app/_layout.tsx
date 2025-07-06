2d6473dc4cef7d19d85870440e3f107121b8bcf5
Yuvi dew <yuvraj@truelink.ai>
2025-07-06 18:12:13 +0530

    fix : remove the import of globle css fle path

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { SplashScreen, Stack } from "expo-router";
 import { useFonts } from 'expo-font';
 import { useEffect } from "react";
+import "../global.css"
 
 export default function RootLayout() {
     const [loaded] = useFonts({
