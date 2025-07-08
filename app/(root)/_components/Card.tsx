9c1bc9359756529a7c59b89b52ac607c44a9512f
Yuvi dew <yuvraj@truelink.ai>
2025-07-08 06:20:14 +0530

    fix : changing same styling

--- app/(root)/_components/Card.tsx
+++ app/(root)/_components/Card.tsx
@@ -61,17 +61,17 @@ const FeaturedCards = ({
                 style={FeaturedCardsStyles.containerImg}
             />
 
-            <Text 
-                style = {{
-                    paddingHorizontal : 14,
-                    paddingVertical : 5,
-                    borderRadius : 100,
-                    backgroundColor : "#fff",
-                    position : "absolute",
-                    top : 10,
-                    right : 5,
-                    fontFamily : "Jakarta-Medium",
-                    color : colors.primary[700]
+            <Text
+                style={{
+                    paddingHorizontal: 14,
+                    paddingVertical: 5,
+                    borderRadius: 100,
+                    backgroundColor: "#fff",
+                    position: "absolute",
+                    top: 10,
+                    right: 5,
+                    fontFamily: "Jakarta-Medium",
+                    color: colors.primary[700]
                 }}
             >
                 {tripsContent.price}
@@ -81,9 +81,9 @@ const FeaturedCards = ({
                 <View style={FeaturedCardsStyles.absoluteOverlayBox}>
                     {/* Your overlay content */}
                     <Text style={FeaturedCardsStyles.absoluteOverlayHeading}>{tripsContent.name}</Text>
-                    <View style = {{display : "flex" , alignItems : "center" , flexDirection : "row" , gap : 3}}>
-                    <Image source={icons.location} style = {{width : 15 , height : 15}} tintColor={colors.secondary[700]} />
-                    <Text style={FeaturedCardsStyles.absoluteOverlayText} >{tripsContent.location.split(",")[0]}</Text>
+                    <View style={{ display: "flex", alignItems: "center", flexDirection: "row", gap: 3 }}>
+                        <Image source={icons.location} style={{ width: 15, height: 15 }} tintColor={colors.secondary[700]} />
+                        <Text style={FeaturedCardsStyles.absoluteOverlayText} >{tripsContent.location.split(",")[0]}</Text>
                     </View>
                 </View>
             </View>
@@ -126,7 +126,7 @@ const FeaturedCardsStyles = StyleSheet.create({
         paddingVertical: 12,
         borderTopRightRadius: 0,
         width: "100%",
-        gap : 6
+        gap: 6
     },
 
     absoluteOverlayHeading: {
@@ -137,7 +137,7 @@ const FeaturedCardsStyles = StyleSheet.create({
 
     absoluteOverlayText: {
         fontFamily: "Jakarta",
-        fontSize: 15,
+        fontSize: 13,
         color: colors.secondary[600],
     },
 
@@ -166,4 +166,165 @@ const FeaturedCardsStyles = StyleSheet.create({
         color: colors.secondary[600],
     },
 });
+
+
+export const Cards = ({
+    id,
+    onPress,
+    imageUrl,
+    result
+}: FeaturedCardsPropsType) => {
+    const [tripsContent, setTripsContent] = useState({
+        name: "",
+        location: "",
+        price: 0
+    })
+    useEffect(() => {
+        const tripDetail = (() => {
+            if (!result) return null;
+
+            try {
+                const cleaned = result.replace(/```json|```/g, "").trim();
+
+                return JSON.parse(cleaned);
+            } catch (error) {
+                console.error("❌ JSON Parse Error:", error);
+                return null;
+            }
+        })();
+
+        if (tripDetail) {
+            setTripsContent({
+                name: tripDetail?.name,
+                location: tripDetail?.itinerary?.slice(0, 2).map((day: any) => day.location).join(", ") || "No locations",
+                price: tripDetail?.estimatedPrice
+            })
+        }
+
+    }, [])
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+            style={styles.container}
+        >
+            {/* <View style={styles.ratingBox}>
+                <Image source={icons.star} style={styles.starIcon} />
+                <Text style={styles.ratingText}>{rating}</Text>
+            </View> */}
+
+            <Image
+                source={{ uri: imageUrl[0] }}
+                style={styles.cardImage}
+            />
+
+            <View style={styles.details}>
+                <Text style={styles.name}>{tripsContent.name}</Text>
+                <View style={{ display: "flex", alignItems: "center", flexDirection: "row", gap: 3 }}>
+                    <Image source={icons.location} style={{ width: 15, height: 15 }} tintColor={colors.secondary[700]} />
+                    <Text style={FeaturedCardsStyles.absoluteOverlayText} >{tripsContent.location.split(",")[0]}</Text>
+                </View>
+
+                <View style={styles.bottomRow}>
+                    <Text style={styles.price}>{tripsContent.price}</Text>
+                    <Image
+                        source={icons.heart}
+                        style={styles.heartIcon}
+                        tintColor="#191d31"
+                    />
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: "100%",
+        marginTop: 16,
+        paddingHorizontal: 12,
+        paddingVertical: 16,
+        backgroundColor: "#fff",
+        flexDirection: "row",
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "#D1D5DB",
+        position: "relative",
+        gap: 10
+    },
+
+    ratingBox: {
+        flexDirection: "row",
+        alignItems: "center",
+        backgroundColor: "rgba(255,255,255,0.95)",
+        paddingHorizontal: 12,
+        paddingVertical: 4,
+        borderRadius: 999,
+        zIndex: 50,
+        position: "absolute",
+        top: 12,
+        right: 12,
+    },
+
+    starIcon: {
+        width: 14,
+        height: 14,
+    },
+
+    ratingText: {
+        fontSize: 12,
+        fontWeight: "bold",
+        fontFamily: "Jakarta-Bold",
+        color: "#1C1917", // stone-900
+        marginLeft: 2,
+    },
+
+    cardImage: {
+        width: "40%",
+        height: 160,
+        borderRadius: 8,
+    },
+
+    details: {
+        flexDirection: "column",
+        marginTop: 8,
+        width: "60%",
+        height: "auto",
+        justifyContent: "space-between"
+    },
+
+    name: {
+        fontFamily: "Jakarta-SemiBold",
+        fontSize: 16,
+        color: colors.primary[600],
+    },
+
+    address: {
+        fontSize: 14,
+        color: colors.secondary[600],
+        fontFamily: "Jakarta-Medium",
+    },
+
+    bottomRow: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+
+        marginTop: 8,
+    },
+
+    price: {
+        fontSize: 16,
+        fontFamily: "Jakarta-Medium",
+        color: "#60A5FA", // blue-400
+    },
+
+    heartIcon: {
+        width: 20,
+        height: 20,
+        marginRight: 8,
+    },
+});
+
+
 
