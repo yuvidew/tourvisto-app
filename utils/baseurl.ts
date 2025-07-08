dedb8fec16389403911e6d5a20be5f7d51edd1b6
Yuvi dew <yuvraj@truelink.ai>
2025-07-08 06:21:52 +0530

    fix : adding a new api

--- utils/baseurl.ts
+++ utils/baseurl.ts
@@ -4,5 +4,6 @@ const IP = "http://192.168.1.2:2000/v1"
 export const baseURL = {
   sign_in: `${IP}/user/auth/sign-in`,
   sign_up: `${IP}/user/auth/sign-up`,
-  get_trips : `${IP}/trip/get-all-trips`
+  get_trips : `${IP}/trip/get-all-trips`,
+  get_trips_by_id : `${IP}/trip/get-trip-by-id`
 };
