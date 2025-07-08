998da65badd0b15c462f9477f4db0f189df4d8fe
Yuvi dew <yuvraj@truelink.ai>
2025-07-08 06:19:37 +0530

    fix : adding a navigation route

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,33 +4,48 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { icons } from '../../../constants/icons';
 import { colors } from '../../../assets/colors';
 import Filter from '../_components/Filter';
-import FeaturedCards from '../_components/Card';
+import FeaturedCards, { Cards } from '../_components/Card';
 import { useGetTrips } from '../hooks/useGetTrips';
 import Search from '../_components/Search';
-import { Link } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 // import { useGetTrips } from '../hooks/useGetTrips';
 
 const Home = () => {
     // const {loading , } = useGetTrips()
-    const {loading, trips , responseMsg , showModal ,} = useGetTrips()
+    const router = useRouter()
+    const { loading, trips, responseMsg, showModal, } = useGetTrips();
+
     return (
         <SafeAreaView style={styles.container}>
             <FlatList
-                data={[1, 2, 3, 4]}
-                renderItem={(item) => <Text>{item.index}</Text>}
+                data={trips}
+                renderItem={({ item }) =>
+                    <Cards
+                        imageUrl={item?.images?.split(',').map((img: string) => img.trim()) || ""}
+                        id={item.id}
+                        result={item.result}
+                        onPress={() => { }}
+                    />
+                }
+                keyExtractor={(item) => item.id.toString()}
+                // numColumns={2}
                 showsVerticalScrollIndicator={false}
                 style={{ flex: 1 }}
                 contentContainerStyle={{
-                    paddingBottom: 32,
-                    gap: 20,
+                    paddingBottom: 72,
+                    gap: 5,
                     paddingHorizontal: 20
                 }}
-
+                // columnWrapperStyle = {{
+                //     display : "flex",
+                //     gap : 5,
+                //     paddingVertical : 20
+                // }}
                 ListEmptyComponent={
                     loading ? (
                         <ActivityIndicator
                             size={"large"}
-                            style = {styles.LoaderStyle}
+                            style={styles.LoaderStyle}
                         />
                     ) : null
                 }
@@ -43,13 +58,13 @@ const Home = () => {
                                 <Text style={styles.headerText1}>Traveling today?</Text>
                             </View>
                             <View>
-                                <Image style = {{width : 40 , height : 40 , objectFit : "cover"}} source={icons.man3}  />
+                                <Image style={{ width: 40, height: 40, objectFit: "cover" }} source={icons.man3} />
                             </View>
                         </View>
                         {/* end to header  */}
 
                         {/* start to search */}
-                        <Search/>
+                        <Search />
                         {/* end to search */}
 
                         {/* start to filter trips card  */}
@@ -57,12 +72,12 @@ const Home = () => {
                         {/* end to filter trips card  */}
 
                         {/* start to popular destination */}
-                        <View style = {styles.popularStandBox}>
-                            <Text style = {styles.popularStandHeadline}>
+                        <View style={styles.popularStandBox}>
+                            <Text style={styles.popularStandHeadline}>
                                 Popular Destination
                             </Text>
 
-                            <Link href= "/" style = {styles.popularSeeMore}>
+                            <Link href="/" style={styles.popularSeeMore}>
                                 See all
                             </Link>
                         </View>
@@ -71,12 +86,15 @@ const Home = () => {
                         {/* start filter trips by interests */}
                         <FlatList
                             data={trips}
-                            renderItem={({item}) => 
+                            renderItem={({ item }) =>
                                 <FeaturedCards
-                                    id = {item.id}
                                     imageUrl={item?.images?.split(',').map((img: string) => img.trim()) || []}
+                                    id={item.id}
                                     result={item.result}
-                                    onPress={() => {}}
+                                    onPress={() => router.push({
+                                        pathname: "/(root)/trip-details/[id]",
+                                        params: { id: item.id }
+                                    })}
                                 />
                             }
                             keyExtractor={(item) => String(item.id)}
@@ -90,6 +108,18 @@ const Home = () => {
                             }}
                         />
                         {/* end filter trips by interests */}
+
+                        {/* start to trips list  */}
+                        <View style={styles.popularStandBox}>
+                            <Text style={styles.popularStandHeadline}>
+                                All Destination
+                            </Text>
+
+                            <Link href="/" style={styles.popularSeeMore}>
+                                See all
+                            </Link>
+                        </View>
+                        {/* end to trips list  */}
                     </View>
                 }
             />
@@ -105,16 +135,16 @@ const styles = StyleSheet.create({
         flexDirection: "column",
     },
 
-    LoaderStyle : {
-        color : colors.primary[700],
-        marginTop : 28
+    LoaderStyle: {
+        color: colors.primary[700],
+        marginTop: 28
     },
 
     header: {
         display: "flex",
         flexDirection: "row",
         alignItems: "center",
-        justifyContent : "space-between"
+        justifyContent: "space-between"
     },
 
     headerTextCont: {
@@ -138,23 +168,23 @@ const styles = StyleSheet.create({
         height: 38
     },
 
-    popularStandBox : {
-        display : "flex",
-        alignItems : "center",
-        justifyContent : "space-between",
-        flexDirection : "row"
+    popularStandBox: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row"
     },
 
-    popularStandHeadline : {
+    popularStandHeadline: {
         fontFamily: "Jakarta-Medium",
         fontSize: 22,
         color: colors.secondary[900],
     },
 
-    popularSeeMore : {
-        fontFamily : "Jakarta-Medium",
-        fontSize : 14,
-        color : colors.primary[800]
+    popularSeeMore: {
+        fontFamily: "Jakarta-Medium",
+        fontSize: 14,
+        color: colors.primary[800]
     }
 })
 
